b70e80cf78d46133d2d1715509136c9bba9d87ed
Mr-Aman26 <99275306+Mr-Aman26@users.noreply.github.com>
2024-07-30 12:38:52 +0530

    added the login page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full h-[100vh] flex items-center justify-center">
+      Hey there I am the dashboard you are looking for. Please Edit me.
+    </div>
+  );
+};
+
+export default page;
